cb276f70ad9989bfa61405653b8e03c68938199b
roovix <yourtom936@gmail.com>
2024-10-22 23:25:33 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const name = "Roovix Shop";
+const firebaseConfig = {
+    apiKey: "AIzaSyA9_Xtlduo-sI6cCwwpnRDBTEA-13i0aQI",
+    authDomain: "ec-affiliate.firebaseapp.com",
+    projectId: "ec-affiliate",
+    storageBucket: "ec-affiliate.appspot.com",
+    messagingSenderId: "166080127246",
+    appId: "1:166080127246:web:a21b27512218783db3775f",
+    measurementId: "G-BEW9MPV4SC"
+};
